d59126e77b53cd7804698727158641ff636f1eb6
loldruger <loldruger@gmail.com>
2018-10-23 23:47:52 +0900

    Update CircularQueue.c

--- DataStructure/CircularQueue.c
+++ DataStructure/CircularQueue.c
@@ -2,15 +2,16 @@
 
 bool IsEmpty(CircularQueue* this);
 bool IsFull(CircularQueue* this);
-bool Enqueue(CircularQueue* this, void* data);
-void* Dequeue(CircularQueue* this);
+bool Enqueue(CircularQueue* this, intptr_t* data);
+intptr_t* Dequeue(CircularQueue* this);
 
-CircularQueue* new_CircularQueue(uint16_t size)
+CircularQueue* new_CircularQueue(size_t size)
 {
 	CircularQueue* this = (CircularQueue*)malloc(sizeof(CircularQueue));
 
 	this->size = size;
-	this->queue = (void**)malloc(sizeof(void*)*size);
+	this->queue = (intptr_t**)malloc(sizeof(intptr_t**)*size);
+
 	this->front = 0;
 	this->rear = 0;
 
@@ -19,13 +20,22 @@ CircularQueue* new_CircularQueue(uint16_t size)
 	this->Enqueue = &Enqueue;
 	this->Dequeue = &Dequeue;
 
+#ifdef _DEBUG_
+	printf("Instance circular queue has been created.\n");
+	printf("Queue Size is %ld.\n", sizeof(this->queue[0]));
+#endif
 	return this;
 }
 
 bool IsEmpty(CircularQueue* this)
 {
     if(this->front == this->rear)
-        return true;
+    {
+#ifdef _DEBUG_
+    	printf("Queue is empty.\n");
+#endif
+    	return true;
+    }
     else
     	return false;
 }
@@ -33,28 +43,41 @@ bool IsEmpty(CircularQueue* this)
 bool IsFull(CircularQueue* this)
 {
     if((this->rear + 1) % this->size == this->front)
-        return true;
+    {
+#ifdef _DEBUG_
+    	printf("Queue is full.\n");
+#endif
+    	return true;
+    }
     else
         return false;
 }
 
-bool Enqueue(CircularQueue* this, void* data)
+bool Enqueue(CircularQueue* this, intptr_t* data)
 {
 	if(this->IsFull(this))
-		return false;
+	    return false;
 
 	this->rear = (this->rear + 1) % this->size;
 	this->queue[this->rear] = data;
 
+#ifdef _DEBUG_
+    printf("Enqueued %p at %p\n", this->queue[this->rear], &this->queue[this->rear]);
+#endif
+
 	return true;
 }
 
-void* Dequeue(CircularQueue* this)
+intptr_t* Dequeue(CircularQueue* this)
 {
 	if(this->IsEmpty(this))
-		return NULL;
+	    return NULL;
 
 	this->front = (this->front + 1) % this->size;
+
+#ifdef _DEBUG_
+	printf("Dequeued %p from %p\n", this->queue[this->front], &this->queue[this->front]);
+#endif
 
 	return this->queue[this->front];
 }
